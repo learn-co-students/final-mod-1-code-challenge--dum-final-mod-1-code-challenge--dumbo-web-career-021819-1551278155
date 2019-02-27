357f1bb1198fbd4f55936e8d296387e01a88b34c
campbelllssoup <matjocampbell@gmail.com>
2019-02-27 10:43:03 -0500

    Creates the Movie, QueueItem, and Viewer classes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,15 +1,72 @@
 class Movie
   attr_accessor :title
 
-  @@all = []
+  @@movies = []
 
   def initialize(title)
     @title = title
-    self.class.all << self
+    @@movies << self
   end
 
   def self.all
-    @@all
+    @@movies
+  end
+
+  def queue_items
+    QueueItem.all.select do |item|
+      item.movie == self
+    end
+  end
+
+  def viewers
+    self.queue_items.map do |item|
+      item.viewer
+    end
+  end
+
+  def average_rating
+    total_rating = 0
+    queue_items.each do |item|
+      if item.rating == nil
+        # do nothing
+      end
+      item.rating = item.rating.to_f
+      total_rating += item.rating
+    end
+
+    return total_rating / queue_items.count
+  end
+
+  def self.highest_rated
+    no_nils = QueueItem.all.select do |item|
+      item.rating != nil
+    end
+
+    lo_to_hi = no_nils.sort_by do |item|
+      item.rating
+    end
+
+    hi_to_lo = lo_to_hi.reverse
+    return hi_to_lo[0].movie
   end
 
 end
+
+#+ `Movie.all`
+#  + returns an array of all `Movie` WORKS!!!
+
+#+ `Movie#queue_items`
+#  + returns an array of all the `QueueItem` instances that contain this movie
+# WORKS !!!!
+
+#+ `Movie#viewers`
+#  + returns an array of all of the `Viewer`s with this `Movie` instance in
+# their queue
+# WORKS !!!
+
+#+ `Movie#average_rating`
+#  + returns the average of all ratings for this instance of `Movie` WORKS !!!
+
+
+#+ `Movie.highest_rated`
+#  + returns the instance of `Movie` with the highest average rating WORKS !!!
--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,4 +1,37 @@
 class QueueItem
+  attr_accessor :rating
+  attr_reader :viewer, :movie
+  @@items = []
 
+  def initialize(movie,viewer,rating=nil)
+    @movie = movie
+    @viewer = viewer
+    @rating = rating
+    @@items << self
+  end
+
+  def self.all
+    @@items
+  end
 
 end
+
+
+### `QueueItem`
+
+#_A `QueueItem` object represents that an individual user has added a particular
+#movie to their queue, and possibly rated it. **It is not the full queue, but a
+#single item in the queue.**_
+
+#+ `QueueItem.all`
+#  + returns an array of all `QueueItem`s WORKS!!!
+
+#+ `QueueItem#viewer`
+#  + returns the viewer associated with this `QueueItem` WORKS !!!
+
+#+ `QueueItem#movie`
+#  + returns the movie associated with this `QueueItem` WORKS!!!
+
+#+ `QueueItem#rating`
+#  + returns the rating for this `QueueItem`. If the viewer has not yet rated
+# the movie, `QueueItem#rating` should be `nil` WORKS!!!
