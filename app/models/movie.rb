3c3f00531b1d61892e2a578f9938897915dd39d0
Rhohoman <rhohoman@gmail.com>
2019-02-27 10:43:32 -0500

    phew

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,11 +5,42 @@ class Movie
 
   def initialize(title)
     @title = title
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
+  end
+
+
+
+  def queue_items
+    QueueItem.all.select{|item| item.title == self}
+  end
+
+  def viewers
+    queue_items.collect{|item| item.username}
+  end
+
+
+  def average_rating
+    sum = 0
+    array_avg = 0
+    average_rating =0
+    array_avg = viewers.collect{|movie| sum = sum + movie.rating}
+
+    average_rating = sum.to_f/array_avg.length
+  end
+
+
+
+
+  def self.highest_rating
+
+  end
+
+  def self.highest_rated
+
   end
 
 end
