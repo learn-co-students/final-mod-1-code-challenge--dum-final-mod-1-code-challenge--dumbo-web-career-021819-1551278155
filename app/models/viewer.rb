e244ed27206ecc203e45cfaf602a5a2a16a71a9d
1-8192 <allegranzia@gmail.com>
2019-02-27 10:42:55 -0500

    did the coding challenged

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -8,8 +8,33 @@ class Viewer
     self.class.all << self
   end
 
+  def queue_items
+    QueueItem.all.select{|item| item.viewer == self}
+  end
+
+  def queue_movies
+    queue_items.map {|item| item.movie}
+  end
+
+  def add_movie_to_queue(movie)
+    QueueItem.new(self, movie)
+  end
+
+  def rate_movie(movie, rating)
+    bool = false
+    queue_items.map do |item|
+      if item.movie == movie
+        item.rating = rating
+        bool = true
+      end
+    end
+    if bool == false
+      QueueItem.new(self, movie, rating)
+    end
+  end
+
   def self.all
     @@all
   end
-  
+
 end
