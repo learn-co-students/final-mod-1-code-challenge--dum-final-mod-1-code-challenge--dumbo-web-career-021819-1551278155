e45f54df08aadc2d127c7c59b6f3543863025c06
merestrickland <mere.strickland@gmail.com>
2019-02-27 10:43:01 -0500

    Code challenge submission

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,34 @@ class Movie
     @@all
   end
 
+  def queue_items
+    QueueItem.all.select do |queue_instance|
+      queue_instance.movie == self
+    end
+  end
+
+  def viewers
+    queue_items.map do |queue_item|
+      queue_item.viewer
+    end
+  end
+
+  def average_rating
+    
+  end
+
+  def self.highest_rated
+
+  end
+
+
 end
+
+
+#
+# + `Movie#average_rating`
+#   + returns the average of all ratings for this instance of `Movie`
+#
+#
+# + `Movie.highest_rated`
+#   + returns the instance of `Movie` with the highest average rating
--- tools/console.rb
+++ tools/console.rb
@@ -4,9 +4,19 @@ def reload
   load 'config/environment.rb'
 end
 
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+meredith = Viewer.new("meredith")
+jael = Viewer.new("jael")
+
+right_thing = QueueItem.new(jael, "Do the Right Thing", 4)
+
+rafiki = Movie.new("Rafiki")
+
+watch_rafiki = QueueItem.new(meredith, rafiki)
+
+titanic = QueueItem.new(meredith, "Titanic", 3)
+
+
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
