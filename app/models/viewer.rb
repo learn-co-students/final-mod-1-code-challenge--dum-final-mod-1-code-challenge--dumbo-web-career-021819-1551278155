f1423cab1c3b2933f5629bdc3efe2661aaf4a2eb
brianryu <s.brianryu@gmail.com>
2019-02-27 10:43:03 -0500

    first code challenge complete

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,36 @@ class Viewer
   def self.all
     @@all
   end
-  
+
+  def queue_items
+    QueueItem.all.select do |item|
+      item.viewer == self
+    end
+  end
+
+  def queue_movies
+    queue_items.map do |item|
+      item.movie
+    end
+  end
+
+  def add_movie_to_queue(movie)
+    QueueItem.new(movie, self)
+  end
+
+  def rate_movie(movie, rating)
+    queue_items.map do |item|
+      if item.movie == movie
+        item.rating = rating
+      end
+
+      if item.include?(movie) == false
+        QueueItem.new(movie, self, rating)
+      else
+        break
+      end
+    end
+  end
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,5 +8,26 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+rushhour = Movie.new("RushHour")
+fastandfurious = Movie.new("FastAndFurious")
+americanpie = Movie.new("AmericanPie")
+
+brianryu = Viewer.new("brianryu")
+hsunice = Viewer.new("hsunice")
+jtona = Viewer.new("jtona")
+dannyjamz = Viewer.new("dannyjamz")
+
+firstq = QueueItem.new(rushhour, brianryu, 5)
+secondq = QueueItem.new(fastandfurious, brianryu, 5)
+thirdq = QueueItem.new(americanpie, hsunice, 3)
+fourthq = QueueItem.new(rushhour, jtona, 4)
+fifthq = QueueItem.new(americanpie, dannyjamz, 4)
+
+# brianryu.queue_items
+# brianryu.queue_movies
+
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
